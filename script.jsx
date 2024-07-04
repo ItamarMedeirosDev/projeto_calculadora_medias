0db1f9c70d552f5ec0719ff584bf22f48cc89e03
Itamar Medeiros <itamarjunior@outlook.com.br>
2024-07-04 15:11:24 -0300

    dizimas

--- script.jsx
+++ script.jsx
@@ -48,7 +48,7 @@ function attTabela(){
 function attMediaFinal(){
     const mediaFinal=calculaMediaFinal();
 
-    document.getElementById('media-final-valor').innerHTML = mediaFinal;
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2);
     document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;   
     
 }
